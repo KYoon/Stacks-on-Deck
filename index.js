6dc45d40bafe92c5c27b921cc58a0cf13d7850db
Flori Garcia-Vicente & John N. Mandalakas <pair+fgv02009.jnmandal@devbootcamp.com>
2015-03-06 17:13:44 -0600

    install express-ejs-layout and add layout file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // core frameworks
 var express = require('express');
+var layouts = require('express-ejs-layouts');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -8,6 +9,7 @@ var _ = require('underscore');
 // templates
 app.set('views', './views')
 app.set('view engine', 'ejs')
+app.use(layouts);
 
 // middleware
 var bodyParser = require('body-parser');
