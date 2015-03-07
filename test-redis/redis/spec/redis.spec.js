4cbe2df143ae0a9845431f408aafc1a620d95414
Aaron Harris & Brian Sheridan <pair+aamharris.bsheridan12@devbootcamp.com>
2015-03-06 19:17:19 -0600

    fix path

--- test-redis/redis/spec/redis.spec.js
+++ test-redis/redis/spec/redis.spec.js
@@ -1,11 +1,11 @@
-var redis = require("./index");
+var test = require("./test");
 
 describe("Redis", function(){
   var gameId = 123
 
   describe("When creating a deck name", function(){
     it("should concact :deck on the end of the gameId", function(){
-      expect(redis.deckName(gameId)).toEqual("123:deck")
+      expect(deckName(gameId)).toEqual("123:deck")
     });
   });
 
