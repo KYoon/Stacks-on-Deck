6d23e2e9f20778b9afbcb3c3d30d9ea3399cab0a
Flori Garcia-Vicente & John N. Mandalakas <pair+fgv02009.jnmandal@devbootcamp.com>
2015-03-08 11:04:18 -0500

    lower case card values from redis, change templates to all lower case

--- public/views/card_view.js
+++ public/views/card_view.js
@@ -1,7 +1,8 @@
 var CardView = Backbone.View.extend({
   initialize: function(card) {
     this.suit = card.attributes.suit;
-    this.value = card.attributes.value;
+    this.value = card.attributes.value.toLowerCase();
+    console.log('templates/'+ this.value +'.tpl')
     this.color = cardColor(this.suit);
     this.cardTemplate = window['JST']['templates/'+ this.value +'.tpl'];
   },
