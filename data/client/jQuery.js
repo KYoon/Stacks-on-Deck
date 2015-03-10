282d58fecfed59208f699e11c4880754b93959dc
Brian Sheridan <bsheridan12@gmail.com>
2015-03-10 18:37:56 -0500

    working card with counter to set handsize

--- data/client/jQuery.js
+++ data/client/jQuery.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
   var toUser = "";
 
   // JQuery Calls
-  $("form").on("submit", function(e){
-    e.preventDefault();
-    var dealingCount = $(this).find("#initial-deal-count").val();
-    var faceDown = $("#facedown").is(':checked');
-    dealCards({dealingCount: dealingCount, cardAppearance: faceDown});
-  });
+  // $("form").on("submit", function(e){
+  //   e.preventDefault();
+  //   var dealingCount = $(this).find("#initial-deal-count").val();
+  //   var faceDown = $("#facedown").is(':checked');
+  //   dealCards({dealingCount: dealingCount, cardAppearance: faceDown});
+  // });
 
   $(".passing-player-list").on("click", ".pass-to", function(e){
     e.preventDefault();
@@ -19,4 +19,22 @@ $(document).ready(function(){
   $("#draw-card").click(function(){
     socket.emit("drawCard")
   });
+
+  $(".down-arrow").on("click", function(){
+    var count = parseInt($("#count").text())
+    if (count > 0) {
+      $("#count" ).html(count - 1)
+    }
+  })
+
+  $(".up-arrow").on("click", function(){
+    var count = parseInt($("#count").text())
+    $("#count").html(count + 1)
+  })
+
+  $("#start-game-btn").on("click", function() {
+    var dealingCount = parseInt($("#count").text())
+    // var faceDown = $("#facedown").is(':checked');
+    dealCards({dealingCount: dealingCount});
+  })
 });
--- public/socket_magic.js
+++ public/socket_magic.js
@@ -12,12 +12,12 @@ $(document).ready(function(){
   var toUser = "";
 
   // JQuery Calls
-  $("form").on("submit", function(e){
-    e.preventDefault();
-    var dealingCount = $(this).find("#initial-deal-count").val();
-    var faceDown = $("#facedown").is(':checked');
-    dealCards({dealingCount: dealingCount, cardAppearance: faceDown});
-  });
+  // $("form").on("submit", function(e){
+  //   e.preventDefault();
+  //   var dealingCount = $(this).find("#initial-deal-count").val();
+  //   var faceDown = $("#facedown").is(':checked');
+  //   dealCards({dealingCount: dealingCount, cardAppearance: faceDown});
+  // });
 
   $(".passing-player-list").on("click", ".pass-to", function(e){
     e.preventDefault();
@@ -29,6 +29,24 @@ $(document).ready(function(){
   $("#draw-card").click(function(){
     socket.emit("drawCard")
   });
+
+  $(".down-arrow").on("click", function(){
+    var count = parseInt($("#count").text())
+    if (count > 0) {
+      $("#count" ).html(count - 1)
+    }
+  })
+
+  $(".up-arrow").on("click", function(){
+    var count = parseInt($("#count").text())
+    $("#count").html(count + 1)
+  })
+
+  $("#start-game-btn").on("click", function() {
+    var dealingCount = parseInt($("#count").text())
+    // var faceDown = $("#facedown").is(':checked');
+    dealCards({dealingCount: dealingCount});
+  })
 });
 
 function cardColor(suit){
