d3be8671049c2f8f839365aaa5055827b295512d
Aaron Harris <aamharris@gmail.com>
2015-03-09 19:46:59 -0500

    debugging after implementing working json objects

--- data/socket.js
+++ data/socket.js
@@ -20,8 +20,16 @@ io.on('connection', function(socket){
     });
   });
 
+  // need to use inside every getHand callback
+  function jsonParser(data) {
+    var jsonCards = [];
+    for ( i=0; i < data.length; i++) {
+      jsonCards.push(JSON.parse(data[i]))
+    }
+    return jsonCards;
+  }
+
   socket.on("dealCards", function(data){
-    // need to transfer the data of faceDown card attribute to cards
     var roomKey = socket.rooms[1];
     repo.createDeck(roomKey);
     repo.dealUsersCards(roomKey, parseInt(data.dealingCount));
@@ -31,11 +39,7 @@ io.on('connection', function(socket){
       socketKeys.forEach(function(key){
         repo.getUser(roomKey, key, function(err, username){
           repo.getHand(roomKey, username, function(err, data){
-            console.log("data" + data)
-            for ( i=0; i < data.length; i++) {
-              var jsonObject = (JSON.parse(data[i]))
-              io.to(key).emit("updateHand", jsonObject);
-            }
+              io.to(key).emit("updateHand", jsonParser(data));
           })
         })
       })
@@ -50,7 +54,8 @@ io.on('connection', function(socket){
       socketKeys.forEach(function(key){
         repo.getUser(roomKey, key, function(err, username){
           repo.getHand(roomKey, username, function(err, data){
-            io.to(key).emit("updateHand", data.sort());
+
+            io.to(key).emit("updateHand",  jsonParser(data));
           })
         })
       })
@@ -61,7 +66,7 @@ io.on('connection', function(socket){
     var roomKey = socket.rooms[1];
     repo.dealUserCard(roomKey, socket.username);
     repo.getHand(roomKey, socket.username, function(err, data){
-      io.to(socket.id).emit("updateHand", data.sort());
+      io.to(socket.id).emit("updateHand",  jsonParser(data));
     })
   })
 
@@ -74,7 +79,7 @@ io.on('connection', function(socket){
         var socketKeys = keys
         socketKeys.forEach(function(key){
           repo.getHand(roomKey, "Table", function(err, data){
-            io.to(key).emit("updateTable", data);
+            io.to(key).emit("updateTable",  jsonParser(data));
           })
         })
       })
@@ -91,7 +96,7 @@ io.on('connection', function(socket){
           var socketKeys = keys
           socketKeys.forEach(function(key){
             repo.getHand(roomKey, "Table", function(err, data){
-              io.to(key).emit("updateTable", data);
+              io.to(key).emit("updateTable",  jsonParser(data));
             })
           })
         })
@@ -103,7 +108,7 @@ io.on('connection', function(socket){
     var roomKey = socket.rooms[1];
     repo.passCard(roomKey, socket.username, "Discard", card);
     repo.getHand(roomKey, socket.username, function(err, data){
-      io.to(socket.id).emit("updateHand", data);
+      io.to(socket.id).emit("updateHand",  jsonParser(data));
     })
   })
 
@@ -117,7 +122,7 @@ io.on('connection', function(socket){
           var socketKeys = keys
           socketKeys.forEach(function(key){
             repo.getHand(roomKey, "Table", function(err, data){
-              io.to(key).emit("updateTable", data);
+              io.to(key).emit("updateTable",  jsonParser(data));
             })
           })
         })
@@ -132,7 +137,7 @@ io.on('connection', function(socket){
       var socketKeys = keys
       socketKeys.forEach(function(key){
         repo.getHand(roomKey, "Table", function(err, data){
-          io.to(key).emit("updateTable", data);
+          io.to(key).emit("updateTable",  jsonParser(data));
         })
       })
     })
