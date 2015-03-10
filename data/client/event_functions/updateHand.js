7a27037ad1d5d7a8477c850b1311c92f4c97790b
Brian Sheridan <bsheridan12@gmail.com>
2015-03-10 12:00:20 -0500

    add staic table size after deal

--- data/client/event_functions/updateHand.js
+++ data/client/event_functions/updateHand.js
@@ -2,7 +2,7 @@ function updateHand(data){
   $(".dealing-cards").hide();
   $('#draw-card').show();
   $(".waiting-room").remove();
-
+  $(".table-container").css( "height", "280px")
   hand.updateCards(data)
   handView.render();
 }
--- public/socket_magic.js
+++ public/socket_magic.js
@@ -83,7 +83,7 @@ function updateHand(data){
   $(".dealing-cards").hide();
   $('#draw-card').show();
   $(".waiting-room").remove();
-
+  $(".table-container").css( "height", "280px")
   hand.updateCards(data)
   handView.render();
 }
