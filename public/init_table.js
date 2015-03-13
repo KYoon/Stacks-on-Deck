c36c5dd968096b843b7e26c34d8e5b34eca52077
John N. Mandalakas <jnmandal@users.noreply.github.com>
2015-03-12 21:39:29 -0500

    add instantiation of messaging view of table init

--- public/init_table.js
+++ public/init_table.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
 
+  // instantiate table views
   table = new Table();
   tableView = new TableView({collection: table});
   $(".tableclass").append(tableView.$el);
+
+  // instantiate message view
+  messageView = new MessageView();
 
   // listen for drag
   socket.on("addCardToTable", function(){
